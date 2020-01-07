381bf01058f60ae05b35a0f03343ca27d0941233
Matteo Tafani Alunno <tafax@users.noreply.github.com>
2020-01-07 23:11:07 +0100

    chore(root): update dependencies (#4)
    fix #2

--- scripts/publish-dry-run.sh
+++ scripts/publish-dry-run.sh
@@ -6,7 +6,5 @@ echo "Run a publish in dry run mode..."
 
 source ./.env
 
-# Allows to login to NPM using environment variables.
-./node_modules/.bin/npm-cli-login
 # Release a new version and publish to NPM.
-yarn release-it:debug
+yarn semantic-release --debug
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,11 +9,8 @@ if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]] || [[ $TRAVIS_BRANCH != "master" ]]
 fi
 
 # Makes sure Travis CI is able to push to github.
-git remote set-url origin https://tafax:$GITHUB_TOKEN@github.com/tafax/layerr.git
 git checkout master
 git pull --tags origin master
 
-# Allows to login to NPM using environment variables.
-./node_modules/.bin/npm-cli-login
 # Release a new version and publish to NPM.
-yarn release-it
+yarn semantic-release
