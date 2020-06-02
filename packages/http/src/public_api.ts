dd846b7491d41d7487ec4cdcf99a46c5ed438f98
Matteo Tafani Alunno <matteo.tafanialunno@gmail.com>
2020-06-02 12:28:04 +0200

    feat(http): allows a request to specify a base host
    Allows to specify a per request base host, so that any request can potentially triggers a different
    api set.
    
    fix #53

--- packages/http/src/public_api.ts
+++ packages/http/src/public_api.ts
@@ -4,9 +4,37 @@ import 'ts-polyfill/lib/es2015-core';
 import 'ts-polyfill/lib/es2015-promise';
 import 'ts-polyfill/lib/es2015-collection';
 
+// Adapter
+export { HttpAdapterInterface } from './adapter/http-adapter.interface';
+
+// Error
+export { HttpLayerrErrorType } from './error/http-layerr.error-type';
+export { HttpLayerrError } from './error/http-layerr.error';
+
+// Middleware
+export { ErrorExecutionBusMiddleware } from './middleware/error-execution.bus-middleware';
+export { RequestExecutionBusMiddleware } from './middleware/request-execution.bus-middleware';
+export { RequestHandlerBusMiddleware } from './middleware/request-handler.bus-middleware';
+export { HttpBackendBusMiddleware } from './middleware/http-backend/http-backend.bus-middleware';
+export { HttpBackend } from './middleware/http-backend/decorator/http-backend';
+
+// Pagination
+export { PaginatedResult } from './pagination/paginated-result';
+export { PaginationInterface } from './pagination/pagination.interface';
+
 // Requests
 export { AbstractRestfulRequest } from './request/abstract-restful.request';
 export { RequestInterface } from './request/request.interface';
+
+// Request handler
+export { RequestHandlerInterface } from './request-handler/request-handler.interface';
+
+// Response
+export { ErrorRemoteResponse } from './response/error-remote-response';
+export { RemoteResponse } from './response/remote-response';
+
+// Service
+export { RequestExecutor } from './service/request-executor';
 
 // Utilities
 export { HttpHeaders } from './utilities/http-headers';
