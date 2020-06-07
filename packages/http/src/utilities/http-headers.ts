5f2540c94067c289e62ccc58bc7c24d00fa074a4
Matteo Tafani Alunno <matteo.tafanialunno@gmail.com>
2020-06-07 21:41:31 +0200

    feat(http): add new method to headers object
    We need to introduce new method in the headers object to allow it to convert itself into an object.
    This will help the consumer to convert the headers object to be processed into the call.
    
    fix #60

--- packages/http/src/utilities/http-headers.ts
+++ packages/http/src/utilities/http-headers.ts
@@ -7,7 +7,31 @@ export class HttpHeaders implements Headers {
   /**
    * The map to use to store values.
    */
-  private _map: Map<string, string> = new Map();
+  private _map: Map<string, string>;
+
+  constructor(headers?: HttpHeaders | string[][] | Record<string, string>) {
+
+    this._map = new Map();
+
+    if (!headers) {
+      return;
+    }
+
+    if (Array.isArray(headers)) {
+      this._map = new Map(headers as unknown as readonly [ string, string ][]);
+      return;
+    }
+
+    if (headers instanceof HttpHeaders) {
+      headers.forEach(
+        (value: string, key: string) => this._map.set(key, value)
+      );
+      return;
+    }
+
+    const headersInit = headers as Record<string, string>;
+    Object.keys(headersInit).forEach((key: string) => this._map.set(key, (headersInit[key])));
+  }
 
   /**
    * @inheritDoc
@@ -53,6 +77,20 @@ export class HttpHeaders implements Headers {
       (value, key) => callbackfn(value, key, this),
       thisArg
     );
+  }
+
+  /**
+   * Converts the headers into a JSON object.
+   */
+  toObject(): Record<string, string> {
+    let obj = {};
+    for (const key of Array.from(this._map.keys())) {
+      obj = {
+        ...obj,
+        [key]: this._map.get(key)
+      };
+    }
+    return obj;
   }
 
 }
